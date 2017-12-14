d7477be4b6a5db1ea19074182801162952fbddd8
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 11:42:36 -0800

    adding more tests, error handling for inputs that are not parens.

--- proper_parenthetics.py
+++ proper_parenthetics.py
@@ -3,20 +3,24 @@
 
 def proper_parenthetics(string):
     """Return if parentheses are matching or not."""
-    paren_list = list(string)
-    opening_parens = 0
-    closing_parens = 0
-    import pdb; pdb.set_trace()
-    for i in paren_list:
-        if i == '(' and closing_parens == 0:
-            opening_parens += 1
-        elif i == '(' and closing_parens > 0:
-            closing_parens -= 1
-        elif i == ')' and opening_parens == 0:
-            return -1
-        elif i == ')' and opening_parens > 0:
-            opening_parens -= 1
-    if opening_parens - closing_parens == 0:
-        return 0
-    if opening_parens - closing_parens > 0:
-        return 1
+    if isinstance(string, str):
+        paren_list = list(string)
+        opening_parens = 0
+        closing_parens = 0
+        for i in paren_list:
+            # import pdb; pdb.set_trace()
+            if i != '(' and i != ')':
+                raise TypeError('proper_parenthetics takes only parentheses.')
+            if i == '(' and closing_parens == 0:
+                opening_parens += 1
+            elif i == '(' and closing_parens > 0:
+                closing_parens -= 1
+            elif i == ')' and opening_parens == 0:
+                return -1
+            elif i == ')' and opening_parens > 0:
+                opening_parens -= 1
+        if opening_parens - closing_parens == 0:
+            return 0
+        if opening_parens - closing_parens > 0:
+            return 1
+    raise TypeError('proper_parenthetics takes only strings')
