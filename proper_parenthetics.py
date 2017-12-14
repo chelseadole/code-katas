be0ec7a9c555f419d273f4662483f52bbb907d0f
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 12:03:25 -0800

    passing all tests, Queue now integrated.

--- proper_parenthetics.py
+++ proper_parenthetics.py
@@ -6,11 +6,13 @@ from _que_structure import Q
 def proper_parenthetics(string):
     """Return if parentheses are matching or not."""
     if isinstance(string, str):
-        paren_q = Q(list(string))
+        paren_q = Q()
+        for i in range(len(string)):
+            paren_q.enqueue(string[i])
         opening_parens = 0
         closing_parens = 0
-        while paren_q.size() > 0:
-            i = paren_q.dequeue().data
+        while paren_q.size() > 0 and paren_q.queue.head is not None:
+            i = paren_q.dequeue()
             if i != '(' and i != ')':
                 raise TypeError('proper_parenthetics takes only parentheses.')
             if i == '(' and closing_parens == 0:
